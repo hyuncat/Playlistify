c24ca38115c8ece78898f0d2a46a1d0b0ad31d92
Sarah Hong <ssh2198@columbia.edu>
2024-04-02 15:05:35 +0000

    fixed db_url

--- playlistify/db_config.py
+++ playlistify/db_config.py
@@ -6,7 +6,7 @@ import psycopg2
 DB_USERNAME = os.getenv('DATABASE_USERNAME')
 DB_PASSWORD = os.getenv('DATABASE_PASSWORD')
 DB_HOST = os.getenv('DATABASE_HOST')
-DATABASE_URI = f"postgresql://{DB_USERNAME}:{DB_USERNAME}@{DB_HOST}/proj1part2"
+DATABASE_URI = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/proj1part2"
 
 # Create a database engine that knows how to connect to the URI above.
 my_engine = create_engine(DATABASE_URI)
